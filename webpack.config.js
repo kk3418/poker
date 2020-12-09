84f16b6a16244529a09409f1059028e55981f07d
kevin <kevin345618@outlook.com>
2020-12-09 21:44:39 +0800

    remove  no use require

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: "./src/main.js",
